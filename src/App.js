178b815dfa6c1fa1412827e72033a35b05919d6c
rahul-mhaskar <rahulmhaskar999@gmail.com>
2025-07-13 02:10:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./app.css";
+import "./App.css";
 
 function App() {
   const [ipos, setIpos] = useState([]);

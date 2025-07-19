fcce0163ff5cecde3f20876060350c537a91d3a9
rahul-mhaskar <rahulmhaskar999@gmail.com>
2025-07-19 10:13:24 +0530

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,1 +1,11 @@
+import React from "react";
+
+export function Input({ className = "", ...props }) {
+  return (
+    <input
+      className={`rounded border px-3 py-2 text-sm ${className}`}
+      {...props}
+    />
+  );
+}
 

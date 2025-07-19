f7b9660a65cbff9223d22cc3a33924025f2b2a6d
rahul-mhaskar <rahulmhaskar999@gmail.com>
2025-07-19 10:37:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Card } from "./components/ui/card";   // ✅ CORRECT
+import { Card } from "./components/ui/card";
 
 import { Input } from "./components/ui/input";
 import { Switch } from "./components/ui/switch";

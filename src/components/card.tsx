8169f5df7d460c6e051bfb0043dcecde76a3a389
rahul-mhaskar <rahulmhaskar999@gmail.com>
2025-07-19 10:51:51 +0530

    Create card.tsx

---
+++ src/components/card.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+export function Card({ className = "", children, ...props }) {
+  return (
+    <div className={`rounded-xl bg-white p-4 shadow ${className}`} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ className = "", children, ...props }) {
+  return (
+    <div className={`text-gray-800 text-sm ${className}`} {...props}>
+      {children}
+    </div>
+  );
+}

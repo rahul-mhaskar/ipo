72d0f3dbd568310bafd6f63130c8e08417409520
rahul-mhaskar <rahulmhaskar999@gmail.com>
2025-08-06 01:03:22 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,21 @@
+export const GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSHEORz3aArzaDTOWYW6FlC1avk1TYKAhDKfyALmqg2HMDWiD60N6WG2wgMlPkvLWC9d7YzwplhCStb/pub?output=csv";
+
+export const BROKER_LINKS = [
+  {
+    name: "Zerodha",
+    href: "https://zerodha.com/open-account?c=VCB643",
+    logo: "https://zerodha.com/static/images/logo.svg",
+  },
+  {
+    name: "Upstox",
+    href: "https://upstox.onelink.me/0H1s/4LAYGW",
+    logo: "https://assets.upstox.com/website/images/upstox-new-logo.svg",
+  },
+  {
+    name: "Paytm Money",
+    href: "https://paytmmoney.page.link/DSwSvdhoasovQYLz9",
+    logo: "https://play-lh.googleusercontent.com/nXCY9Did341stoQEhCEH5wJW2FBybZYbpiYl2J-eCajYOXZ_XXXHX1ptjATuA0zayg",
+  },
+];
+
+export const CONTACT_EMAIL = 'trackmyipo@outlook.com';
